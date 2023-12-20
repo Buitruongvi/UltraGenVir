c3384e3f22157df10ee93e76f93c03cc6840b047
BUI TRUONG VI <49474873+Buitruongvi@users.noreply.github.com>
2023-12-20 23:31:15 +0700

    update

--- Module/Tokenizer.py
+++ Module/Tokenizer.py
@@ -24,33 +24,37 @@ class Tokenizer(object):
         for idx, token in enumerate(vocab):
             token2idx[token] = idx + 1
             idx2token[idx + 1] = token
+
         return token2idx, idx2token
 
     @staticmethod
     def clean_report(report):
         report = re.sub(r'\.{2,}', '.', report)
         report = re.sub(r'\s?\d+\.\s?', '. ', report)
-        report = re.sub(r'[.,?;*!%^&_+():-\[\]{}]', '',report.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower())
+        report = re.sub(r'[.,?;*!%^&_+():-\[\]{}]', '',
+                        report.replace('"', '').replace('/', '').replace('\\', '').replace("'", '').strip().lower())
         tokens = re.split(r'\. |\s|-', report)
         tokens = [token for token in tokens if token]
-        #cleaned_report = ' . '.join(tokens) + ' .'
+        # cleaned_report = ' . '.join(tokens) + ' .'
         cleaned_report = ' '.join(tokens)
         return cleaned_report
 
-    def get_token_by_id(self):
-        return self.token2idx
+    def get_vocab_size(self):
+        return len(self.token2idx)
 
-    def get_idx_by_token(self):
-        return self.idx2token
+    def get_token_by_id(self, id):
+        return self.idx2token[id]
+
+    def get_idx_by_token(self, token):
+        if token not in self.token2idx:
+            return self.token2idx['<unk>']
+        return self.token2idx[token]
 
     def decoder(self, ids):
-        txt = ''
-        for i, idx in enumerate(ids):
-            if idx > 0:
-                if i>=1:
-                    txt += ' '
-                txt += self.idx2token[idx]
-            else:
-                break
+        txt_tokens = [
+            self.idx2token[idx] if idx > 0 and idx in self.idx2token
+            else self.idx2token[self.token2idx['<unk>']]
+            for idx in ids
+        ]
+        txt = ' '.join(txt_tokens)
         return txt
-
